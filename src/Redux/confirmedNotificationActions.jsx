3890cc55eed0097ca3968425a983c462ad7d8119
Mohamed Hichem Rahmani <hichem1029@live.fr>
2024-12-11 01:12:56 +0100

    Reset Redux

--- src/Redux/confirmedNotificationActions.jsx
+++ src/Redux/confirmedNotificationActions.jsx
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+/* import { createSlice } from '@reduxjs/toolkit';
 import { notification_load_limit } from '../Utils/Parameters.jsx';
 
 const confirmedNotificationsSlice = createSlice({
@@ -33,6 +33,45 @@ const confirmedNotificationsSlice = createSlice({
         },
         loadMore: (state, action) => {
             // Adjust the index and slice to load more data
+            state.data = state.data.slice(0, action.payload + notification_load_limit);
+            state.index = action.payload + state.index;
+        },
+    },
+});
+
+// Export actions
+export const { getConfirmedNotifications, confirmedNotificationSuccess, confirmedNotificationFail, loadMore } = confirmedNotificationsSlice.actions;
+
+// Export reducer
+export default confirmedNotificationsSlice.reducer;
+ */
+
+import { createSlice } from '@reduxjs/toolkit';
+import { notification_load_limit } from '../Utils/Parameters.jsx';
+
+const confirmedNotificationsSlice = createSlice({
+    name: 'confirmedNotifications',
+    initialState: {
+        isLoading: true,
+        data: [],
+        error: '',
+        index: notification_load_limit
+    },
+    reducers: {
+        getConfirmedNotifications: (state) => {
+            state.isLoading = true;
+        },
+        confirmedNotificationSuccess: (state, action) => {
+            state.isLoading = false;
+            state.data = action.payload;
+            state.error = '';
+        },
+        confirmedNotificationFail: (state, action) => {
+            state.isLoading = false;
+            state.data = [];
+            state.error = action.payload;
+        },
+        loadMore: (state, action) => {
             state.data = state.data.slice(0, action.payload + notification_load_limit);
             state.index = action.payload + state.index;
         },
